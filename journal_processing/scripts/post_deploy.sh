d1722495844a26ede7bcbe90a9ef2214c9c4723a
Clive Holloway <clive.holloway@gmail.com>
2025-08-05 12:36:23 -0400

    Tweak deploy

--- journal_processing/scripts/post_deploy.sh
+++ journal_processing/scripts/post_deploy.sh
@@ -8,5 +8,10 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# build out the pages
 python ./journal_processing/scripts/extract.py \
     data/HerbertHollowayJournals.docx
+
+# update the build dir (used by the website)
+rm -rf ~/build
+mv ./journal_processing/build ~/build
