d7e122e274c4646fb3deec66abae0b6d0d9d61fd
Clive Holloway <clive.holloway@gmail.com>
2025-08-24 20:23:35 -0400

    Fix typo in deploy script

--- .github/scripts/post_deploy.sh
+++ .github/scripts/post_deploy.sh
@@ -4,10 +4,10 @@ set -e
 cd "$(dirname "$0")/../.."
 
 # configure the script environment
-[ -d .venv ] || python3 -m venv .venv
+[ -d venv ] || python3 -m venv venv
 source .venv/bin/activate
 pip install --upgrade pip
-pip install -r .
+pip install -e .
 
 # build out the pages and data.json
 herbert extract data/HerbertHollowayJournals.docx
